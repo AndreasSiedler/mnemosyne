da0c96d3e6d806604a8f44942cc34e366f6da9b7
Andreas Siedler <andreas.siedler@gmail.com>
2022-12-25 12:02:13 +0100

    Update add post component

--- components/post/AddPost.tsx
+++ components/post/AddPost.tsx
@@ -1,32 +1,27 @@
 import { Controller, useForm } from "react-hook-form";
 import React, { createRef, useEffect, useState } from "react";
-import { API, graphqlOperation } from "aws-amplify";
+import { API } from "aws-amplify";
 import {
   FormErrorMessage,
   FormControl,
-  Input,
   Button,
   Flex,
   useToast,
-  Image,
-  Spacer,
   Progress,
   Container,
-  IconButton,
   Center,
   Icon,
 } from "@chakra-ui/react";
 import { toastPosition } from "../../config/constants";
-import isEmpty from "lodash/isEmpty";
 import { RichTextEditor } from "../texteditor/RichtextEditor";
 import { useRouter } from "next/router";
 import { BsLock } from "react-icons/bs";
 import { useMutation } from "@tanstack/react-query";
 import { POSTS_NEW_ROUTE } from "../../pages/posts/new";
-import { createEntry, createPost } from "../../graphql/mutations";
-import Calendar from "./Calendar";
+import { createPost } from "../../graphql/mutations";
 import ImageManager from "../ImageManager";
 import { Image as TImage } from "../../API";
+import { GRAPHQL_AUTH_MODE } from "@aws-amplify/api";
 
 const formSteps = ["mood", "content"];
 
@@ -66,13 +61,15 @@ export default function AddPost() {
 
   const { mutate, isLoading } = useMutation(
     (data: ICreatePostInput) => {
-      return API.graphql<any>(
-        graphqlOperation(createPost, {
+      return API.graphql<any>({
+        query: createPost,
+        authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
+        variables: {
           input: {
             content: JSON.stringify(data.content),
           },
-        })
-      );
+        },
+      });
     },
     {
       onError: () =>
@@ -108,7 +105,7 @@ export default function AddPost() {
    * Create post with data
    * @param {ICreatePostInput} data
    */
-  async function handleFurther(data: ICreatePostInput): Promise<void> {
+  async function onSubmit(data: ICreatePostInput): Promise<void> {
     if (isLastStep) {
       const postData: any = {
         images: data.images,
@@ -119,14 +116,6 @@ export default function AddPost() {
       router.push(`${POSTS_NEW_ROUTE}?step=${activeStep + 1}`);
     }
   }
-
-  const onClickUploadFile = () => {
-    uploadInputRef.current?.click();
-  };
-
-  const handleBack = () => {
-    router.push(`${POSTS_NEW_ROUTE}?step=${activeStep - 1}`);
-  };
 
   return (
     <>
@@ -139,7 +128,7 @@ export default function AddPost() {
         <Center>
           <Icon as={BsLock} h={"10"} w={"10"} color={"gray.300"} />
         </Center>
-        <form onSubmit={handleSubmit(handleFurther)} noValidate>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <FormControl isInvalid={Boolean(errors.images)} isRequired>
             <ImageManager
               {...register("images")}
