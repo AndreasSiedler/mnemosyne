78d5ba2e8735b359871cd25d9693cf12402caecb
Andreas Siedler <andreas.siedler@gmail.com>
2022-12-26 22:40:14 +0100

    Update post item

--- components/post/PostItem.tsx
+++ components/post/PostItem.tsx
@@ -1,26 +1,31 @@
-import Image from "next/image";
-import {
-  Box,
-  Center,
-  Heading,
-  Text,
-  Stack,
-  Avatar,
-  useColorModeValue,
-  IconButton,
-} from "@chakra-ui/react";
+import { Box, Center, useColorModeValue, IconButton } from "@chakra-ui/react";
 import { Post } from "../../API";
 import { DeleteIcon } from "@chakra-ui/icons";
 import { BiPencil } from "react-icons/bi";
 import { useRouter } from "next/router";
 import DynamicImage from "../DynamicImage";
 import { first } from "lodash";
+import { Editable, Slate, withReact } from "slate-react";
+import { useMemo } from "react";
+import { createEditor } from "slate";
+
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: "This example shows what happens when the Editor is set to readOnly, it is not editable",
+      },
+    ],
+  },
+];
 
 type PostItemProps = {
   post: Post;
 };
 
 export default function PostItem({ post }: PostItemProps) {
+  const editor = useMemo(() => withReact(createEditor()), []);
   const router = useRouter();
   const params = router.query;
   const bigImage = first(post.images?.items);
@@ -35,45 +40,21 @@ export default function PostItem({ post }: PostItemProps) {
         maxW={"445px"}
         w={"full"}
         bg={useColorModeValue("white", "gray.900")}
-        boxShadow={"lg"}
+        boxShadow={"md"}
         rounded={"md"}
         p={6}
         overflow={"hidden"}
       >
         <Box h={"210px"} bg={"gray.100"} mt={-6} mx={-6} mb={6} pos={"relative"}>
           {bigImage && <DynamicImage imageKey={bigImage?.fullSize.key as string} />}
-
           <Box position={"absolute"} right={0} p={2}>
             <IconButton icon={<DeleteIcon />} aria-label={"Delete post"} mr={1} />
             <IconButton icon={<BiPencil />} aria-label={"Delete post"} onClick={handlePostEdit} />
           </Box>
         </Box>
-        <Stack>
-          <Text
-            color={"green.500"}
-            textTransform={"uppercase"}
-            fontWeight={800}
-            fontSize={"sm"}
-            letterSpacing={1.1}
-          >
-            Blog
-          </Text>
-          <Heading
-            color={useColorModeValue("gray.700", "white")}
-            fontSize={"2xl"}
-            fontFamily={"body"}
-          >
-            Boost your conversion rate
-          </Heading>
-          <Text color={"gray.500"}>{post.content}</Text>
-        </Stack>
-        <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
-          <Avatar src={"https://avatars0.githubusercontent.com/u/1164541?v=4"} />
-          <Stack direction={"column"} spacing={0} fontSize={"sm"}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={"gray.500"}>Feb 08, 2021 · 6min read</Text>
-          </Stack>
-        </Stack>
+        <Slate editor={editor} value={JSON.parse(post.content ?? "")}>
+          <Editable readOnly placeholder="Enter some plain text..." />
+        </Slate>
       </Box>
     </Center>
   );
