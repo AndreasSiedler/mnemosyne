8801571825045cf12c39723f0767951fa66309fd
Andreas Siedler <andreas.siedler@gmail.com>
2022-12-28 13:33:59 +0100

    Add image loading skeleton

--- components/DynamicImage/index.tsx
+++ components/DynamicImage/index.tsx
@@ -1,9 +1,9 @@
 import { Storage } from "aws-amplify";
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import { Skeleton } from "@chakra-ui/skeleton";
-import useSWR from "swr";
 import Image from "next/image";
 import { Alert } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 
 interface DynamicImageProps {
   imageKey: string;
@@ -15,11 +15,22 @@ interface DynamicImageProps {
  * @return {ReactElement}
  */
 export default function DynamicImage({ imageKey }: DynamicImageProps): ReactElement {
-  const { data, error } = useSWR(`/dynamicImage/${imageKey}`, () => fetcher(imageKey));
+  const [isLoaded, setIsLoaded] = useState(false);
+  const { data, error } = useQuery([`/dynamicImage/${imageKey}`], () => fetcher(imageKey));
 
   if (error) return <Alert>Something went wrong</Alert>;
-  if (!data) return <Skeleton />;
-  return <Image alt="" src={data} fill style={{ objectFit: "cover" }} />;
+  if (!data) return <Skeleton h={"210px"} />;
+  return (
+    <Skeleton isLoaded={isLoaded} h={"210px"}>
+      <Image
+        onLoadingComplete={() => setIsLoaded(true)}
+        alt=""
+        src={data}
+        fill
+        style={{ objectFit: "cover" }}
+      />
+    </Skeleton>
+  );
 }
 
 const fetcher = async (key: string): Promise<string> => {
